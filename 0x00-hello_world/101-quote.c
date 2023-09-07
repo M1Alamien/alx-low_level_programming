bb63e39a9bfc334fa83ac3ac37d90d28ac4fb328
M1Alamien <m.alamien0@gmail.com>
2023-09-07 21:50:21 +0300

    m

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - certain output
+ *
+ * outputs to the standard error 
+ * a certain line
+ * Return: 1 always
+ */
+int main()
+{
+	fprintf(stder, "and that piece of art is usefu\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
