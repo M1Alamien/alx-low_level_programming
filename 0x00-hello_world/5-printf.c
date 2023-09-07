27868ef250ab6464000c44c6dd7fe0b852b16fc0
M1Alamien <m.alamien0@gmail.com>
2023-09-07 20:56:31 +0300

    m

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main: certain output
+ *
+ * outputs using printf function this time 
+ * Return: 0 all the time
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n";
+	return (0);
+}
