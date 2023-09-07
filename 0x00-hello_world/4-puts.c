ee51bba82d0bd1e4f19c65596d68ca8a09602622
M1Alamien <m.alamien0@gmail.com>
2023-09-07 20:30:43 +0300

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@ int main(void)
 {
 	char str1[53];
 
-	strcpy(str1, ""Programming is like building a multilingual puzzle");
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
 	puts(str1);
 		return (0);
 }
