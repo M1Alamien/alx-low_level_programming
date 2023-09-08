b050a22fe9033db6ccde5684c083efeef3d8a13c
M1Alamien <m.alamien0@gmail.com>
2023-09-08 16:26:21 +0300

    m

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int hex;
+
+	for (hex = 0; hex <= 9; hex++)
+	{
+		putchar('0' + hex);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c)
+	}
+	putchar('\n');
+	return (0);
+}
