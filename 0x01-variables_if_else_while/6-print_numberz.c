48ef328a997e7b7de8a7568b62a6301e107349aa
M1Alamien <m.alamien0@gmail.com>
2023-09-08 15:58:01 +0300

    m

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - 0 to 9
+ *
+ * outputs numbers 0 to 9 with certain
+ * conditions to meet
+ * Return: always 0
+ */
+int main (void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar('0' + n);
+	}
+	putchar('\n');
+	return (0);
+}
