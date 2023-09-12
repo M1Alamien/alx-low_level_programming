4c1f2c9be9ba1ba6f964e73b3fffba329814ecd4
M1Alamien <m.alamien0@gmail.com>
2023-09-13 00:21:21 +0300

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,8 @@
  */
 int main(void)
 {
-	char s[] = "_putchar";
-	int i;
-
-		for (i = 0; s[i] != '\0'; i++)
-		{
-			_putchar(s[i]);
-		}
+	_putchar('_');
+	_putchar('p');
 	_putchar('\n');
 	return (0);
 }
