dfceb0e6fac764d2771fd2c203c35b5450c2e96e
M1Alamien <m.alamien0@gmail.com>
2023-09-13 01:36:15 +0300

    m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,9 +5,18 @@
  * prints the alphabet
  * Return: always 
  */
-void print_alphabet(void);
+void print_alphabet(void)
+{
+	char s[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+}
 int main(void)
 {
 	print_alphabet();
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,3 @@
  *      * On error, -1 is returned, and errno is set appropriately.
  *       */
 int _putchar(char c);
-void print_alphabet(void);
