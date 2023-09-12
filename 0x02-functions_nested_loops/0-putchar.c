33fdc18f32766ace9dad41817dfcf3d2c1aed319
M1Alamien <m.alamien0@gmail.com>
2023-09-13 00:17:30 +0300

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,10 @@
 #include "main.h"
-#include <unistd.h>
 /**
  * main - output
  *
  * output is _putchar
  * Return: 0 always
  */
-
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
-}
-
 int main(void)
 {
 	char s[] = "_putchar";
