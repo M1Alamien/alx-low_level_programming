ec2f1afb1817eaa9ce6fb5be6731f608cd7d0391
M1Alamien <m.alamien0@gmail.com>
2023-09-12 23:50:59 +0300

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include "main.h"
+#include <unistd.h>
 /**
  * main - output
  *
  * output is _putchar
  * Return: 0 always
  */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char s[] = "_putchar";
+	int i
+
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			_putchar(s[i]);
+		}
 	_putchar('\n');
 	return (0);
 }
