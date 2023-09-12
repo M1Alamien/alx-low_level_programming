542d0df442a8e8d6092b111c5e463b75b70ca80d
M1Alamien <m.alamien0@gmail.com>
2023-09-13 01:52:11 +0300

    m

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,13 @@
  *      * On error, -1 is returned, and errno is set appropriately.
  *       */
 int _putchar(char c);
+void print_alphabet(void)
+{
+	char s[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+}
