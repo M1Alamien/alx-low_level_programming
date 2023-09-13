a744a0cf13c3ffe7de35344eaafc3994d2b039c8
M1Alamien <m.alamien0@gmail.com>
2023-09-13 04:51:26 +0300

    m

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * times_table - table
+ *
+ * outputs times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int t[] = "0123456789";
+	int i;
+	int c;
+	int r;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 0; t[c] != '\0'; c++)
+		{
+			r = i * t[c];
+			_putchar('0' + r);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
