45840c4dade885a0f678d89a597e79879e4c53f1
M1Alamien <m.alamien0@gmail.com>
2023-09-13 05:25:38 +0300

    m

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * add - plus
+ * @a input from main
+ * @b same
+ *
+ * adds two numbers
+ * Return: always 0
+ */
+int add(int a, int b)
+{
+	int sum;
+	int tens;
+	int ones;
+
+	sum = a + b;
+	if (sum >= 10)
+	{
+		tens = sum / 10;
+		ones = sum % 10;
+		_putchar('0' + tens);
+		_putchar('0' + ones);
+		_putchar('\n');
+		return (0);
+	}
+	_putchar('0' + sum);
+	_putchar('\n');
+	return (0);
+}
