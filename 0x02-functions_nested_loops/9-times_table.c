1b2768090bbcb2f490655b378282e3714fbe7b50
M1Alamien <m.alamien0@gmail.com>
2023-09-13 05:13:21 +0300

    m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,6 +9,8 @@ void times_table(void)
 {
 	int i;
 	int c;
+	int tens;
+	int ones;
 	int r;
 
 	for (i = 0; i <= 9; i++)
@@ -16,9 +18,20 @@ void times_table(void)
 		for (c = 0; c <= 9; c++)
 		{
 			r = i * c;
+			if (r > 10)
+			{
+			ones = r % 10;
+			tens = r / 10;
+			_putchar('0' + tens);
+			_putchar('0' + ones);
+			} else
+			{
 			_putchar('0' + r);
-			_putchar(',');
-			_putchar(' ');
+			}
+			if (c != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 		}
 		_putchar('\n');
 	}
