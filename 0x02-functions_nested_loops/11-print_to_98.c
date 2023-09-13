fcdf69bab2c7deb041e0410b164d93b224247bdf
M1Alamien <m.alamien0@gmail.com>
2023-09-13 05:58:39 +0300

    m

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,29 +13,36 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for( i = n; i >= 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
 			if (i != 98)
 			{
 				printf(", ");
+			}
+			else
+			{
+				printf("\n");
 			}
 		}
 	}
-	else if ( n < 98)
+	else if (n < 98)
 	{
-		for (i = n; i <=98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
 			if (i != 98)
 			{
 				printf(", ");
 			}
+			else
+			{
+				printf("\n");
+			}
 		}
-	} 
+	}
 	else
 	{
-		printf("98");
+		printf("98\n");
 	}
-	_putchar('\n');
 }
