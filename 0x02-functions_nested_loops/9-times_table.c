833d880d965fa735949d62f6d12cf1cc12da376c
M1Alamien <m.alamien0@gmail.com>
2023-09-13 05:00:22 +0300

    m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,16 +7,15 @@
  */
 void times_table(void)
 {
-	int t[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int i;
 	int c;
 	int r;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (c = 0; t[c] != '\0'; c++)
+		for (c = 0; c <= 9; c++)
 		{
-			r = i * t[c];
+			r = i * c;
 			_putchar('0' + r);
 			_putchar(',');
 			_putchar(' ');
