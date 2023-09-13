e75e3afd61ae53fdf41f30f2a0e5b53bffcf269d
M1Alamien <m.alamien0@gmail.com>
2023-09-13 05:45:38 +0300

    m

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include <stdio>
+#include "main.h"
+/**
+ * print_to_98 - to 98
+ * @n: input from main
+ *
+ * print to 98 up or down
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for( i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else if ( n < 98)
+	{
+		for (i = n; i <=98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+			       	_putchar(',');
+				_putchar(' ');
+			}
+		}
+	} 
+	else
+	{
+		printf("98");
+	}
+	printf('\n');
+}
