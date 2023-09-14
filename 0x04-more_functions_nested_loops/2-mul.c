9afc7607d6401d9c7bb3f0eec2b866f48b346efa
M1Alamien <m.alamien0@gmail.com>
2023-09-15 02:17:03 +0300

    m

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplies
+ * @a: int from main
+ * @b: int from main
+ *
+ * mutiplies 2 ints from main
+ * Return: always m
+ */
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return (m)
+}
