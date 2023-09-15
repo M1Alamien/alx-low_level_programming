999a798c85198dba4d4bf44cf460a16f376baa67
M1Alamien <m.alamien0@gmail.com>
2023-09-15 05:30:17 +0300

    m

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_triangle - name
+ * @size: int from main
+ *
+ * prints a triangle to size of size
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i;
+	int ii;
+
+	if (size > 0)
+	{
+		for ( i = 1; i <= size; i++)
+		{
+			for (ii = size - i; ii > 0; ii--)
+			{
+				_putchar(' ');
+			}
+			for (ii = 0; ii < i; ii++)
+			{
+				_putchar(35);
+			}
+			if (i == size)
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
