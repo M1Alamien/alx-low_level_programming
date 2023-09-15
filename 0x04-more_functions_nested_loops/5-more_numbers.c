ab2f4a972c26135b3204281c8a542c2ea089ba9e
M1Alamien <m.alamien0@gmail.com>
2023-09-15 03:00:37 +0300

    m

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - 0 to 14
+ *
+ * prints nums 0 to 14
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int ii;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (ii = 0; ii <= 14; i++)
+		{
+			if (ii > 9)
+			{
+				_putchar('0' + ii / 10);
+			}
+			_putchar('0' + ii % 10);
+		}
+		_putchar('\n');
+	}
+}
