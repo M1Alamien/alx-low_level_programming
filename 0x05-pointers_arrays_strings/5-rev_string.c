e0ec90a3362bbf00c2653f185159b31cd4a8fd38
M1Alamien <m.alamien0@gmail.com>
2023-09-19 02:22:20 +0300

    m

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - name
+ * @s: string from main
+ *
+ * reverses a string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char str_save;
+	int i;
+	int ii = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+	for (; i >= 0; i--, ii++)
+	{
+		str_save[ii] = s[i];
+	}
+	for (; ii >= 0, ii--, i++)
+	{
+		s[i] = str_save[ii];
+	}
+}
