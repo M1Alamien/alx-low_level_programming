9fbdf49b6e9c65b8cd639ede7c02145550cb6ddd
M1Alamien <m.alamien0@gmail.com>
2023-09-19 01:58:28 +0300

    m

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - name
+ * @s: string from main
+ *
+ * prints a string in reverse
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
