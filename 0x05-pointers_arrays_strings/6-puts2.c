ad810b539cb51c127357cddfe70605e138e17496
M1Alamien <m.alamien0@gmail.com>
2023-09-19 02:52:20 +0300

    m

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - name
+ * @str: string from main
+ *
+ * prints every other char from string
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+}
