b6c74296e8140d225f1dd6e8801f1b811acaf525
M1Alamien <m.alamien0@gmail.com>
2023-09-19 03:41:53 +0300

    m

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,13 +12,14 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	while ( i < n)
+	while (i < n)
 	{
 		printf("%d", a[i]);
-		if ( i != n - 1)
+		if (i != n - 1)
 		{
 			printf(", ");
 		}
 		i++;
 	}
+	_putchar('\n');
 }
