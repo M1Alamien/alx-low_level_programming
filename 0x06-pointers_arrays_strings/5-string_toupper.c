40c2999dc6b816419a48f65148dd1348f354e44c
M1Alamien <m.alamien0@gmail.com>
2023-09-20 19:27:41 +0300

    m

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,18 @@
 #include "main.h"
-
+/**
+ * string_toupper - name
+ * @s: string from main
+ *
+ * changes all lowercase to uppercase
+ * Return: s
+ */
 char *string_toupper(char *s)
 {
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] > 96 && s[i] < 123)
+		if ((s[i] > 96) && (s[i] < 123))
 		{
 			s[i] = s[i] - 32;
 		}
