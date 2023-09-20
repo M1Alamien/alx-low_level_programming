1b6239f22e44713578cdb72aa181404ef9d54d1e
M1Alamien <m.alamien0@gmail.com>
2023-09-20 19:29:32 +0300

    m

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ * reverse_array - name
+ * @a: array from main
+ * @n: lenght of a
+ *
+ * reverses inputted array
+ * Return: void
+ */
 void reverse_array(int *a, int n)
 {
 	int i;
