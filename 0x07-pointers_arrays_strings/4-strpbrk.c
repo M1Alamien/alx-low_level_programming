b75f1a80b74a2086062c28f3fe4f4f250bb523ab
M1Alamien <m.alamien0@gmail.com>
2023-09-25 17:18:58 +0300

    m

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strpbrk - name
+ * @s: string from main
+ * @accept: string from main
+ *
+ * locates the first occurrence in the string s
+ * of any bytes in accept
+ * Return: s or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (aacept[j] == s[i])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (NULL);
+}
