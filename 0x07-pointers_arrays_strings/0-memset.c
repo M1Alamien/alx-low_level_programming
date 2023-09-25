5b38b6bb1eb3bb9ebde91189028b85d67844fa03
M1Alamien <m.alamien0@gmail.com>
2023-09-25 15:11:31 +0300

    m

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - name
+ * @s: char array from main
+ * @b: char from main
+ * @n: int from main
+ * fills the first n bytes of s with b
+ * Return: pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
