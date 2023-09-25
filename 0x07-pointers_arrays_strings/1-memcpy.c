0bed4df45fee469a9e5e9999d8f9df77ddcd1c5f
M1Alamien <m.alamien0@gmail.com>
2023-09-25 15:34:55 +0300

    m

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - name
+ * @dest: array from main
+ * @src: array from main
+ * @n: int from main
+ * copies n bytes from src to dest
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (s);
+}
