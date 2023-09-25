7886f0603ae6567b551e9291d2d9ec108a973258
M1Alamien <m.alamien0@gmail.com>
2023-09-25 20:04:11 +0300

    m

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_string - name
+ * @s: string from main
+ * @to: string from main
+ *
+ * sets value of pointer to char
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
