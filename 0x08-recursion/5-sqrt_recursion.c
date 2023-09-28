5372d2bf8644fb8462854e1635850b3cbab6d753
M1Alamien <m.alamien0@gmail.com>
2023-09-28 19:47:50 +0300

    m

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * _sqrt_recursion - name
+ * @n: int from main
+ *
+ * finds nat sqr of n
+ * Return: 0 or 1 or -1 or sqr of n
+ */
 int _sqrt_recursion(int n)
 {
 	if (n == 0 || n == 1)
@@ -11,14 +17,21 @@ int _sqrt_recursion(int n)
 		return (squareroot(n, 0));
 	}
 }
-
+/**
+ * squareroot - name
+ * @n: int from main
+ * @x: int = 0
+ *
+ * finds sqr of n from 0 to n/2
+ * return: -1 or squareroot of n
+ */
 int squareroot(int n, int x)
 {
 	if (x > n / 2)
 	{
 		return (-1);
 	}
-	else if ( x * x == n)
+	else if (x * x == n)
 	{
 		return (x);
 	}
