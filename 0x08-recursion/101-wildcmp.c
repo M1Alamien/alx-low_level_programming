05e05682104b87a7210c56dec68366738201c9af
M1Alamien <m.alamien0@gmail.com>
2023-09-28 21:49:50 +0300

    m

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * wildcmp - name
+ * @s1: string from main
+ * @s2: string from main
+ *
+ * compares two strings if they are identical
+ * Return: 1 or 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	else if (*s2 == '*')
+		return (1);
+
+	else if (*s1 != *s2)
+		return (0);
+	else if (*s1 == *s2)
+		wildcmp(s1 + 1, s2 + 1);
+}
