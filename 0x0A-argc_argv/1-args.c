d1d22cfbf7866ff6c28fde2bb545c5e71691bbad
M1Alamien <m.alamien0@gmail.com>
2023-10-02 20:46:39 +0300

    m

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - name
+ * @argc: int from cl
+ * @argv: string from cl
+ *
+ * prints num of arguments from cl
+ * Return: always 0
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
