e4d1919c6740ce608ae46afd3524103e5f975afc
M1Alamien <m.alamien0@gmail.com>
2023-10-02 19:49:06 +0300

    m

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - name
+ * @argc: int from command line
+ * @argv[]; string from command line
+ *
+ * prints the name of the program
+ * Return: always 0
+ */
+int main(argc, argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
