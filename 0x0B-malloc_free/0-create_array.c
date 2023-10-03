3c5181cf3db9ffa2c7ea753856aae92647a75453
M1Alamien <m.alamien0@gmail.com>
2023-10-03 16:55:49 +0300

    m

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,11 +13,11 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int i;
 
-	if (size == 0)
+	if (size <= 0)
 	{
 		return (NULL);
 	}
-	str = (char*) malloc(size);
+	str = (char *) malloc(size);
 	for (i = 0; i < size; i++)
 	{
 		str[i] = c;
