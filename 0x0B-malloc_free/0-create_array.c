460f1eb4ba500b54d6cab465e698b5923c8c3013
M1Alamien <m.alamien0@gmail.com>
2023-10-03 16:48:42 +0300

    m

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_array - name
+ * @size: int from main
+ * @c: char from mian
+ *
+ * creates an array of chars
+ * and initialzes it with c
+ * Return: NULL or pointer to string
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	str = malloc(size);
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	str[i] = '\0';
+	return (str);
+}
