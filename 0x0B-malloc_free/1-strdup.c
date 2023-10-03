d62117d4e997143e2f9062c6e9c529e02415e76e
M1Alamien <m.alamien0@gmail.com>
2023-10-03 17:16:07 +0300

    m

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * _strlen - name
+ * @s: string from main
+ *
+ * return the length of string
+ * Return: i (lenght of string)
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+
+	}
+	return (i);
+}
+/**
+ * _strdup - name
+ * @str: string from main
+ *
+ * returns a pointer to a copy
+ * of the parameter string
+ * Return: NULL or pointer to s
+ */
+char *_strdup(char *str)
+{
+	int i, len;
+	char *s;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	len = _strlen(str) + 1;
+	s = (char *) malloc(len);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		s[i] = str[i];
+	}
+	s[i] = '\0';
+	return (s);
+}
