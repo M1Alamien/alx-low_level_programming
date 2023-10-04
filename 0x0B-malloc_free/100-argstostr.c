69070641b99d5b84d3c246a3c789637a58d94def
M1Alamien <m.alamien0@gmail.com>
2023-10-04 17:41:36 +0300

    m

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,76 @@
+#include "main.h"
+/**
+ * _strcpy - name
+ * @dest: string from main
+ * @src: string from main
+ *
+ * copy and paste a string
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
+
+/**
+ * _strlen - name
+ * @s: string from main
+ *
+ * return the length of string
+ * Return: i (lenght of string)
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		
+	}
+	return (i);
+}
+
+/**
+ * argstostr - name
+ * @ac: int from main
+ * @av: string from main
+ *
+ * concatenates all the arguments of the program
+ * Return: av or NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	int i,j = 0, tlen = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		tlen = tlen + _strlen(av[i]) + 1;
+	}
+	s = malloc(tlen);
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		_strcpy(s + j, av[i]);
+		j = j + _strlen(av[i]) + 1;
+		s[j - 1] = '\n';
+	}
+	s[tlen - 1] = '\0';
+	return (s);
+}
