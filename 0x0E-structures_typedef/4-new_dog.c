e537cb6604811e66462e4db7d3fd1c0d2f57eb2d
M1Alamien <m.alamien0@gmail.com>
2023-10-11 00:25:18 +0300

    m

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,42 @@
 #include <stddef.h>
 #include "dog.h"
 /**
+ * _strlen - name
+ * @s: string from main
+ *
+ * return the length of string
+ * Return: i (lenght of string)
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+
+	}
+	return (i);
+}
+/**
+ * _strcpy - name
+ * @dest: string from main
+ * @src: string from main
+ *
+ * copy and paste a string
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
+/**
  * new_dog - name
  * @name: *char from main
  * @age: float from main
@@ -13,10 +49,15 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	int lenname, lenowner;
+	lenname = _strlen(name);
+	lenowner = _strlen(owner);
 
 	new_dog = malloc(sizeof(dog_t));
-	new_dog->name = name;
+	new_dog->name = malloc((lenname + 1));
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = malloc((lenowner + 1));
+	_strcpy(new_dog->owner, owner);
+	_strcpy(new_dog->name, name);
 	return (new_dog);
 }
