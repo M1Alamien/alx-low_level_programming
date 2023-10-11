b9fcf4aedc2857f4c86cb8c02270e3c408b0f5d5
M1Alamien <m.alamien0@gmail.com>
2023-10-11 22:13:27 +0300

    m

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+/**
+ * int_index - name
+ * @array: array from main
+ * @size: int from main
+ * @cmp: func from main
+ *
+ * searches for an integer
+ * Return: i or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, re;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		re = cmp(array[i]);
+		if (re != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
