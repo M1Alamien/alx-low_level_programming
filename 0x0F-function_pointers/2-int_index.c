0deb51da59b58e717657afa75813fdf8b336397c
M1Alamien <m.alamien0@gmail.com>
2023-10-11 23:15:44 +0300

    m

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, re;
 
+	if (array != NULL && cmp != NULL)
+	{
 	if (size <= 0)
 		return (-1);
-	for (i = 0; i < size; i++)
-	{
-		re = cmp(array[i]);
-		if (re != 0)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			re = cmp(array[i]);
+			if (re != 0)
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1);
