00c5409903dc3fdde1ded648f22b416018a8e3d9
M1Alamien <m.alamien0@gmail.com>
2023-10-11 21:38:14 +0300

    m

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - name
+ * @name: string from main
+ * @f: function from main
+ *
+ * prints a name
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(*name);
+}
