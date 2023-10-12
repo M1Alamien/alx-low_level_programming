8e1af4f0ed34694cf2968c8f730ebf4ff6965cde
M1Alamien <m.alamien0@gmail.com>
2023-10-12 20:17:04 +0300

    m

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,9 +13,9 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, sum = 0;
 	va_list num;
 
+	va_start(num, n);
 	if (n == 0)
 		return (0);
-	va_start(num, n);
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(num, unsigned int);
