4f890d1ca3efdc3bfb84de2e9fa55d1170604629
M1Alamien <m.alamien0@gmail.com>
2023-10-19 22:56:21 +0300

    m

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * add_node - name
+ * @head: first in list
+ * @str: str in list
+ *
+ * adds a new node at the beginning of list
+ * Return: *head or NULL
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	size_t len1;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->str = strdup(str);
+	for(len1 = 0; str[len1]; len1++)
+	{
+
+	}
+	temp->len = len1;
+	temp->next = *head;
+	*head = new;
+	return (*head);
+}
