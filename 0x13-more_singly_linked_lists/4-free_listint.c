21af3d9d5576611b8fb71587cc4f496c5469d625
M1Alamien <m.alamien0@gmail.com>
2023-10-24 17:05:40 +0300

    m

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - name
+ * @head: first in list
+ *
+ * frees list
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tl;
+
+	while ((tl = head) != NULL)
+	{
+		head = head->next;
+		free(head);
+	}
+}
