faf922c8d082baa6a9978d9c5bec0e1f92c24c2e
M1Alamien <m.alamien0@gmail.com>
2023-10-24 17:12:27 +0300

    m

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - name
+ * @head: first in list
+ *
+ * frees list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tl;
+
+	while ((tl = head) != NULL)
+	{
+		head = head->next;
+		free(tl);
+	}
+	head = NULL;
+}
