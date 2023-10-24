ba3d11e620523047a052054684f2543dc20ba312
M1Alamien <m.alamien0@gmail.com>
2023-10-24 18:31:53 +0300

    m

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -5,15 +5,15 @@
  *
  * sums all ns in list
  * Return: sum
- */
+ sum += head->n;*/
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
 
 	while (head != NULL)
 	{
-		head = head->next;
 		sum += head->n;
+		head = head->next;
 	}
 	return (sum);
 }
