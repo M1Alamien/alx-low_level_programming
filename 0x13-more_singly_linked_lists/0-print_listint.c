12b965856547090de91c7a09b52d86c281ec4d72
M1Alamien <m.alamien0@gmail.com>
2023-10-24 16:19:13 +0300

    m

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,9 +12,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-			printf("%d\n", h->n);
-			h = h->next;
-			ncount++;
+		printf("%d\n", h->n);
+		h = h->next;
+		ncount++;
 	}
 	return (ncount);
 }
