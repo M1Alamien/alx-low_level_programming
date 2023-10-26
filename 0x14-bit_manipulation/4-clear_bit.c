3b322000f4deaad527e129da813434e6b4d4e8a4
M1Alamien <m.alamien0@gmail.com>
2023-10-26 23:15:16 +0300

    m

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * clear_bit - name
+ * @n: pointer to n
+ * @index: index
+ *
+ * sets value of bit @index to 0
+ * Return: -1 or 1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index < 64)
+	{
+		i = 1 << index;
+		if (*n & i)
+			*n ^= m;
+		return (1);
+	}
+	return (-1);
+}
