fb609c8387b60290a4591cba81bf3ebb0ddf2126
M1Alamien <m.alamien0@gmail.com>
2023-10-26 23:05:34 +0300

    m

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * set_bit - name
+ * @n: pointer to n
+ * @index: index
+ * stes value of bit @index to 1
+ * Return: -1 or 1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index < 64)
+	{
+		m = 1 << index;
+		*n = *n | m;
+		return (1);
+	}
+	return (-1)
+}
