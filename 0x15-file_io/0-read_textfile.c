8120f389cf7ff96775d82bfa655243158a712012
M1Alamien <m.alamien0@gmail.com>
2023-10-30 20:41:45 +0200

    m

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,30 +3,30 @@
 * read_textfile - name
 * @filename: filename
 * @letters: int
-* 
+*
 * reads a text file and prints the output
 * Return: 0 or num of letters printed
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t w, r, o;
-    char *buff;
+	int fd;
+	ssize_t w, r, o;
+	char *buff;
 
-    if (!filename)
-        return (0);
-    buff = malloc(sizeof(char) * letters);
-    if (!buff)
-        return (0);
-    o = open(filename,  O_RDONLY);
-    r = read(o, buff, letters);
-    w = write(STDOUT_FILENO, buff, r);
-    if (o == -1 || r == -1 || w == -1 || w != r)
-    {
-        free(buff);
-        return (0);
-    }
-    free(buff);
-    close(o);
-    return (w);
+	if (!filename)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+		return (0);
+	o = open(filename,  O_RDONLY);
+	r = read(o, buff, letters);
+	w = write(STDOUT_FILENO, buff, r);
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+	free(buff);
+	return (0);
+	}
+	free(buff);
+	close(o);
+	return (w);
 }
